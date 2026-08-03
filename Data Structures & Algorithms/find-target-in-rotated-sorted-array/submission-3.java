ee06df4adef6e18907d57b0443fe4cffbf87858b
Aman Gairola <amngairola@gmail.com>
2026-08-03 08:53:35 +0530

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return findMin(nums , target);
+    }
+
+    public int findMin(int[] nums , int k) {
+        int n = nums.length;
+        
+
+        int s = 0;
+        int e = n-1;
+       
+        
+        while (s <= e) {
+            
+            int mid = s + (e - s) / 2;
+
+            // if left sorted
+            if(nums[mid]==k) return mid;
+
+            else if (nums[s] <= nums[mid]) {
+                
+                if(k >= nums[s] && k < nums[mid]){
+                    e = mid-1;
+                }else{
+                    s = mid + 1;
+                }
+               
+            }
+            // if right sorted
+            else {
+
+              if(k > nums[mid] && k <= nums[e]){
+                    s = mid + 1;
+                }else{
+                    e = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
